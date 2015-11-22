14f5a472b9688e6f16c4124e47b6c40007a7c4ba
Isaac Ratner <isaacratner@gmail.com>
2015-11-22 15:21:42 -0500

    user can choose initial positions, which are stored. Gray color styling added to chosen positions.

--- client/pre-build/home/home.controller.js
+++ client/pre-build/home/home.controller.js
@@ -7,16 +7,21 @@ app.controller('HomeController', function($scope, $http) {
 	for (var i = 0; i < maxRows; i++){
 	  $scope.rows.push([]);
 	  for (var j =0; j < maxCols; j++){
-	      $scope.rows[i][j] = i + "," + j;
+	  	$scope.rows[i][j] = Array(i,j);
 	  };
 	};
 
 	//allow user to choose positions
 	$scope.counter = 10;
-	var positionsArray = [];
-	$scope.positionChooser = function() {
-
-	}
+	$scope.positionsArray = [];
+	$scope.positionChooser = function(col) {
+		if($scope.positionsArray.indexOf(col) === -1) {
+			$scope.positionsArray.push(col);
+		} else {
+			$scope.positionsArray.splice($scope.positionsArray.indexOf(col), 1);
+		};
+		console.log($scope.positionsArray);
+	};
 
 
 });
