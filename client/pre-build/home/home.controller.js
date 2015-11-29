1f4f98de44f43aaab1a020a193c4e18dcfc1f82a
Isaac Ratner <isaacratner@gmail.com>
2015-11-29 17:38:24 -0500

    back and forth attacking

--- client/pre-build/home/home.controller.js
+++ client/pre-build/home/home.controller.js
@@ -1,5 +1,6 @@
 app.controller('HomeController', function($scope, boardFactory) {
-	$scope.createBoard = function() {	
+
+	$scope.createBoard = function() {
 		$scope.rows = [];
 		var maxRows = 5;
 		var maxCols = 10;
@@ -9,53 +10,55 @@ app.controller('HomeController', function($scope, boardFactory) {
 		  	$scope.rows[i][j] = Array(i,j);
 		  };
 		};
-	}();
 
-	$scope.positionsArray = [];
-	$scope.strikes = [];
+	$scope.userPositions = [];
+	$scope.cpuPositions = [];
 	$scope.missed = [];
 	$scope.hit = [];
 	$scope.boardSet = false;
+		
+	}();
+
+	$scope.posFinder = function(item, array) {
+		if(typeof array === 'undefined') {
+			console.log('go away')
+		} else {
+	    for (var i = 0; i < array.length; i++) {
+	        if (array[i][0] === item[0] && array[i][1] == item[1]) {
+	            return i;
+	        }
+	    }
+	    return -1;
+		}
+	};
 
 	$scope.positionChooser = function(col) {
-		if(col[1] < 5 && $scope.positionsArray.length < 10) {
+		//set user positions
+		if(col[1] < 5 && !$scope.boardSet && ($scope.userPositions.length < 10 || typeof $scope.userPositions === 'undefined')) {
 			boardFactory.sendPosition(col)
 			.then(function(response) {
-				//$scope.positionsArray = arrayCollections.userPositions;
-				console.log(response)
-				$scope.positionsArray = response;
+				//$scope.userPositions = arrayCollections.userPositions;
+				console.log('userPositions:', response.userPositions)
+				$scope.userPositions = response.userPositions;
 			});
-		}	
+		}
+
+		//strike
+		if(col[1] >= 5 && $scope.boardSet && $scope.posFinder(col, $scope.missed) === -1 && $scope.posFinder(col, $scope.hit) === -1) {
+			boardFactory.sendStrike(col)
+			.then(function(response) {
+				console.log('hit and miss obj:', response)
+				$scope.hit = response.hit;
+				$scope.missed = response.missed;
+			});
+		}
 	};
 
 	$scope.submitPositions = function() {
 		$scope.boardSet = true;
 		boardFactory.start()
-		.then(function(compPos) {
-			console.log(compPos);
-		})
-	};
-
-	$scope.posFinder = function(item) {
-		//var array = $scope.positionsArray;
-	    for (var i = 0; i < $scope.positionsArray.length; i++) {
-	        if ($scope.positionsArray[i][0] === item[0] && $scope.positionsArray[i][1] == item[1]) {
-	            return i;
-	        }
-	    }
-	    return -1;
-	};
-
-	$scope.strikeChooser = function(col) {
-		//test - no onclick even
-
-		if(col[1] > 5 && $scope.boardSet) {
-			$scope.strikes.push(col)
-			boardFactory.sendStrike(col)
-			.then(function(arrayCollections) {
-				$scope.missed = arrayCollections.userMissed;
-				$scope.hit = arrayCollections.userHit;
-			});
-		}
-	};
+		.then(function(response) {
+			console.log('cpuPositions:', response.cpuPositions)
+		});
+	}
 });
--- server/api/index.js
+++ server/api/index.js
@@ -6,15 +6,15 @@ var userIsMissed = [];
 var userIsHit = [];
 var cpuPositions = [];
 var cpuIsMissed = [];
-var cputIsHit = [];
-var arrayCollection: {
-	'userPositions': userPositions;
-	'userIsMissed': userIsMissed;
-	'userIsHit': userIsHit;
-	'cpuPositions': cpuPositions;
-	'cpuIsMissed': cpuIsMissed;
-	'cputIsHit': cputIsHit;
-}
+var cpuIsHit = [];
+// var arrayCollection: {
+// 	'userPositions': userPositions;
+// 	'userIsMissed': userIsMissed;
+// 	'userIsHit': userIsHit;
+// 	'cpuPositions': cpuPositions;
+// 	'cpuIsMissed': cpuIsMissed;
+// 	'cpuIsHit': cpuIsHit;
+// }
 
 function posFinder(array, item) {
 	for (var i = 0; i < array.length; i++) {
@@ -29,9 +29,17 @@ function randomInt(min, max) {
 	return Math.floor(Math.random() * (max-min+1) + min);
 };
 
+//need to only strike empty square but always strike
+function striker() {
+	var tempPos = Array(randomInt(0,4), randomInt(0,4));
+	if(posFinder(userIsHit, tempPos) !== -1 || posFinder(userIsMissed, tempPos) !== -1) {
+		tempPos = striker();
+	}
+	return tempPos;
+}
+
 router.put('/position', function(req, res) {
 	//setup
-	console.log(req.body)
 	var coordinates = req.body.position;
 	if(posFinder(userPositions, coordinates) === -1) {
 		userPositions.push(coordinates);
@@ -39,37 +47,48 @@ router.put('/position', function(req, res) {
 		userPositions.splice(posFinder(userPositions, coordinates), 1)
 	}
 	//res.json(arrayCollection);
-	//below working
-	console.log(userPositions)
-	res.json(userPositions)
+	res.json({userPositions: userPositions})
 });
 
 router.post('/position', function(req, res) {
-	console.log(req.body)
+	//receive strike
+	console.log(req.body.position)
+	var coordinates = req.body.position;
 	if(posFinder(cpuPositions, coordinates) === -1) {
-		cputIsHit.push(coordinates);
-	} else {
 		cpuIsMissed.push(coordinates);
-	}
-	var randomPos = Array(randomInt(0,4), randomInt(0,4));
-	if(find(userPositions, randomPos) === -1) {
-		cpuIsMissed.push(randomPos);
 	} else {
-		cputIsHit.push(randomPos);
+		cpuIsHit.push(coordinates);
+	}
+	
+	//check if final strike
+
+	//return strike
+	var randomPos = striker();
+	console.log('attack user:', randomPos)
+
+	//check if final attack
+
+	if(posFinder(userPositions, randomPos) === -1) {
+		userIsMissed.push(randomPos);
+	} else {
+		userIsHit.push(randomPos);
 	}
 	//res.json(arrayCollection);
-	console.log(cputIsHit, cpuIsMissed)
-	res.json({cputIsHit: cputIsHit, cpuIsMissed: cpuIsMissed});
+	res.json({hit:userIsHit.concat(cpuIsHit), missed:userIsMissed.concat(cpuIsMissed)});
 });    
 
 
 router.get('/start', function(req, res) {
+	console.log('userPositions', userPositions)
 	//setup
 	while(cpuPositions.length < 10) {
 	var randomPos = Array(randomInt(0,4), randomInt(5,9));
-		cpuPositions.push(randomPos);
+		if(posFinder(cpuPositions, randomPos) === -1) {
+			cpuPositions.push(randomPos);
+		}
 	}
-	res.json(cpuPositions);
+	console.log('cpuPositions', cpuPositions)
+	res.json({cpuPositions: cpuPositions});
 });
 
 
