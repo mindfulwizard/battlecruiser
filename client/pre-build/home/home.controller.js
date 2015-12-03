f0a14419fb844c418db046099f1b618a256b7d48
Isaac Ratner <isaacratner@gmail.com>
2015-12-02 21:16:22 -0500

    fixed hilarious bug by which missed attacks turned into hits on double click

--- client/pre-build/home/home.controller.js
+++ client/pre-build/home/home.controller.js
@@ -9,23 +9,41 @@ app.controller('HomeController', function($scope, boardFactory) {
 		})
 	}
 
+	$scope.finderFunc = function(array, x, y) {
+		return _.findIndex(array, {'x': x, 'y': y});
+	}
+
 	$scope.selectPosition = function(x, y) {
+		//check status in ifs
+		var index = $scope.finderFunc($scope.positionsArray, x, y);
+		//undo position selection if previously selected
+		if(index>-1) {
+			return $scope.positionsArray.splice(index, 1);
+		}
+
 		if($scope.positionsArray.length < 10) {
 			$scope.positionsArray.push({'x': x, 'y': y});
-		}	
+		}
 	}
 
 	$scope.submitPositions = function() {
 		boardFactory.playerSetup($scope.positionsArray)
 		.then(function(response) {
-			//show cpu ships
+			$scope.positionsArray = null;
 			$scope.game = response.game;
 		})
 	}
 
-	$scope.finderFunc = function(array, x, y) {
-		return _.findIndex(array, {'x': x, 'y': y});
+	$scope.attackPosition = function(status, x, y) {
+		//prevent selecting previously attacked positions
+		if(status === 0 || status === 1){
+			boardFactory.playerMove({'x': x, 'y': y})
+			.then(function(response) {
+				$scope.game = response.game;
+			})
+		}
 	}
+
 
 });
 
